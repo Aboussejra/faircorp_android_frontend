fc47c5b9de683e0ffed239d040690b7ac885176f
Aboussejra <amir.boussejra@etu.emse.fr>
2021-10-15 09:25:14 +0200

    Trying to fix problems updating window activity

--- app/src/main/java/com/faircorp/MainActivity.kt
+++ app/src/main/java/com/faircorp/MainActivity.kt
@@ -22,21 +22,20 @@ const val WINDOW_NAME_PARAM = "com.faircorp.windowname.attribute"
 //        Toast.makeText(this, "You choose $windowName", Toast.LENGTH_LONG).show()
 //    }
 //}
-
 class MainActivity : BasicActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 
-    /** Called when the user taps the button */
-    fun openWindow(view: View) {
-        val windowName = findViewById<EditText>(R.id.txt_window_name).text.toString()
-
-        // Do something in response to button
-        val intent = Intent(this, WindowActivity::class.java).apply {
-            putExtra(WINDOW_NAME_PARAM, windowName)
-        }
-        startActivity(intent)
-    }
+//    /** Called when the user taps the button */
+//    fun openWindow(view: View) {
+//        val windowName = findViewById<EditText>(R.id.txt_window_name).text.toString()
+//
+//        // Do something in response to button
+//        val intent = Intent(this, WindowActivity::class.java).apply {
+//            putExtra(WINDOW_NAME_PARAM, windowName)
+//        }
+//        startActivity(intent)
+//    }
 }
--- app/src/main/java/com/faircorp/WindowActivity.kt
+++ app/src/main/java/com/faircorp/WindowActivity.kt
@@ -8,28 +8,28 @@ import com.faircorp.model.WindowService
 //const val WINDOW_NAME_PARAM = "com.faircorp.windowname.attribute"
 // already defined in mainActivity, so there seems to be a conflict
 class WindowActivity : BasicActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_window)
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        val param = intent.getStringExtra(WINDOW_NAME_PARAM)
-        val windowName = findViewById<TextView>(R.id.txt_window_name)
-        windowName.text = param
-    }
-//val windowService = WindowService()
-//override fun onCreate(savedInstanceState: Bundle?) {
-//    super.onCreate(savedInstanceState)
-//    setContentView(R.layout.activity_windows)
-//    supportActionBar?.setDisplayHomeAsUpEnabled(true)
-//    val id = intent.getLongExtra(WINDOW_NAME_PARAM, 0)
-//    val window = windowService.findById(id)
-//    val param = intent.getStringExtra(WINDOW_NAME_PARAM)
-//    if (window != null) {
-//        findViewById<TextView>(R.id.txt_window_name).text = window.name
-//        findViewById<TextView>(R.id.txt_room_name).text = window.room.name
-//        findViewById<TextView>(R.id.txt_window_current_temperature).text = window.room.currentTemperature?.toString()
-//        findViewById<TextView>(R.id.txt_window_target_temperature).text = window.room.targetTemperature?.toString()
-//        findViewById<TextView>(R.id.txt_window_status).text = window.status.toString()
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_window)
+//        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+//        val param = intent.getStringExtra(WINDOW_NAME_PARAM)
+//        val windowName = findViewById<TextView>(R.id.txt_window_name)
+//        windowName.text = param
 //    }
-//}
+val windowService = WindowService()
+override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_windows)
+    supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    val id = intent.getLongExtra(WINDOW_NAME_PARAM, 0)
+    val window = windowService.findById(id)
+    val param = intent.getStringExtra(WINDOW_NAME_PARAM)
+    if (window != null) {
+        findViewById<TextView>(R.id.txt_window_name).text = window.name
+        findViewById<TextView>(R.id.txt_room_name).text = window.room.name
+        findViewById<TextView>(R.id.txt_window_current_temperature).text = window.room.currentTemperature?.toString()
+        findViewById<TextView>(R.id.txt_window_target_temperature).text = window.room.targetTemperature?.toString()
+        findViewById<TextView>(R.id.txt_window_status).text = window.status.toString()
+    }
+}
 }
